790ab41b6bcc3cc252564f2c676bf268f35a1ed9
ashtanko <shtankopro@gmail.com>
2021-02-05 20:59:03 +0200

    Add solution of two sum problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/two_sum/two_sum_test.go
@@ -0,0 +1,25 @@
+package two_sum
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+
+	t.Run("should work correctly on case [2,7,11,15]", func(t *testing.T) {
+		nums := []int{2, 7, 11, 15}
+		target := 9
+		result := twoSum(nums, target)
+		expected := []int{0, 1}
+		assert.Equal(t, result, expected)
+	})
+
+	t.Run("should work correctly on case [3,2,4]", func(t *testing.T) {
+		nums := []int{3, 2, 4}
+		target := 6
+		result := twoSum(nums, target)
+		expected := []int{1, 2}
+		assert.Equal(t, result, expected)
+	})
+}
