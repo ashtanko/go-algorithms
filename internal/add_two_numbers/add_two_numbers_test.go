b35a26634604148d78fe87a57408963bd6f50cfc
ashtanko <shtankopro@gmail.com>
2021-02-10 22:53:30 +0200

    Add solution of Add Two Numbers problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/add_two_numbers/add_two_numbers_test.go
@@ -0,0 +1,39 @@
+package add_two_numbers
+
+import (
+	. "github.com/ashtanko/go-algorithms/ds/linked_list"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	testCases := []struct {
+		gotList1 []int
+		gotList2 []int
+		want     []int
+	}{
+		{
+			gotList1: []int{2, 4, 3},
+			gotList2: []int{5, 6, 4},
+			want:     []int{7, 0, 8},
+		},
+		{
+			gotList1: []int{0},
+			gotList2: []int{0},
+			want:     []int{0},
+		},
+		{
+
+			gotList1: []int{9, 9, 9, 9, 9, 9, 9},
+			gotList2: []int{9, 9, 9, 9},
+			want:     []int{8, 9, 9, 9, 0, 0, 0, 1},
+		},
+	}
+
+	for _, testCase := range testCases {
+		actual := addTwoNumbers(FromSlice(testCase.gotList1), FromSlice(testCase.gotList2)).ToSlice()
+
+		utils.Checkf(t, is.DeepEqual(actual, testCase.want), testCase)
+	}
+}
