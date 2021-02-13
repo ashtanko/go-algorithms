d16d26ac3d0e99bceb4cb390f64ff1e8353f18b6
ashtanko <shtankopro@gmail.com>
2021-02-13 18:50:33 +0200

    Add solution of max profit problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/max_profit/max_profit_test.go
@@ -0,0 +1,31 @@
+package max_profit
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestMaxProfit(t *testing.T) {
+	testCases := []struct {
+		prices   []int
+		expected int
+	}{
+		{
+			prices:   []int{7, 1, 5, 3, 6, 4},
+			expected: 5,
+		},
+		{
+			prices:   []int{7, 6, 4, 3, 1},
+			expected: 0,
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d", tc.prices)
+		t.Run(name, func(t *testing.T) {
+			actual := maxProfit(tc.prices)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
