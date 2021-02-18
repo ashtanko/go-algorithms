9f868acd0f43112fcca792d5efab3fc62e026834
ashtanko <shtankopro@gmail.com>
2021-02-18 22:51:09 +0200

    Small refactor
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- internal/min_cost_climbing_stairs/climbing_stairs.go
+++ internal/min_cost_climbing_stairs/climbing_stairs.go
@@ -1,19 +1,13 @@
 package min_cost_climbing_stairs
 
+import "github.com/ashtanko/go-algorithms/utils"
+
 func minCostClimbingStairs(cost []int) int {
 	f1, f2 := 0, 0
 	for i := len(cost) - 1; i >= 0; i-- {
-		f0 := cost[i] + min(f1, f2)
+		f0 := cost[i] + utils.Min(f1, f2)
 		f2 = f1
 		f1 = f0
 	}
-	return min(f1, f2)
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	} else {
-		return y
-	}
+	return utils.Min(f1, f2)
 }
