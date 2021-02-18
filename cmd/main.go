ff6e856467004097be67fb8a5f486c19cbb2e01b
ashtanko <shtankopro@gmail.com>
2021-02-18 22:51:42 +0200

    Add playground
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/ashtanko/go-algorithms/internal/lisp"
+	"os"
+)
+
+var (
+	printSExpr = flag.Bool("sexpr", false, "always print S-expressions")
+	stackDepth = flag.Int("depth", 1e5, "maximum call depth; 0 means no limit")
+)
+
+var loading bool
 
 func main() {
-	fmt.Printf("")
+	flag.Parse()
+	lisp.Config(*printSExpr)
+	context := lisp.NewContext(*stackDepth)
+	loading = true
+	for _, file := range flag.Args() {
+		load(context, file)
+	}
+}
+
+// load reads the named source file and parses it within the context.
+func load(context *lisp.Context, file string) {
+	fd, err := os.Open(file)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer fd.Close()
+	//parser := lisp.NewParser(bufio.NewReader(fd))
+	//input(context, parser, "")
 }
