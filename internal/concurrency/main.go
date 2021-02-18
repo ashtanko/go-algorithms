490eb69a9b8cf8b4eaec4ef961eb424db5550d6f
ashtanko <shtankopro@gmail.com>
2021-02-18 22:51:28 +0200

    Concurrent fib
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/concurrency/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+
+	runtime.GOMAXPROCS(10)
+
+	go spinner(100 * time.Millisecond)
+	const n = 50
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
