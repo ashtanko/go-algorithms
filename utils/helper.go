3d534a3a57969f9492a4f8b0d879e2a8576cbd70
ashtanko <shtankopro@gmail.com>
2021-02-18 21:33:36 +0200

    Add Max util
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- utils/helper.go
+++ utils/helper.go
@@ -9,3 +9,19 @@ import (
 func Checkf(t *testing.T, comparison assert.BoolOrComparison, testCase interface{}) {
 	assert.Check(t, comparison, fmt.Sprintf("%+v", testCase))
 }
+
+func Min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
