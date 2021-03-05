da35a04efca1d8a5a344d7d55bdc47197872366e
ashtanko <shtankopro@gmail.com>
2021-03-05 18:46:56 +0200

    Solution of level order
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/level_order/level_order.go
@@ -0,0 +1,21 @@
+package level_order
+
+import (
+	"github.com/ashtanko/go-algorithms/ds/tree"
+)
+
+func levelOrder(root *tree.TreeNode) [][]int {
+	var ans [][]int
+	if root == nil {
+		return ans
+	}
+
+	level, next := tree.Bfs([]*tree.TreeNode{root})
+	ans = append(ans, level)
+
+	for len(next) != 0 {
+		level, next = tree.Bfs(next)
+		ans = append(ans, level)
+	}
+	return ans
+}
---
+++ internal/level_order/level_order_test.go
@@ -0,0 +1,47 @@
+package level_order
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/ds/tree"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestLevelOrder(t *testing.T) {
+	testCases := []struct {
+		tree     *tree.TreeNode
+		expected [][]int
+	}{
+		{
+			tree:     &tree.TreeNode{},
+			expected: [][]int{{0}},
+		},
+		{
+			tree: &tree.TreeNode{
+				Val: 3,
+				Left: &tree.TreeNode{
+					Val: 9,
+				},
+				Right: &tree.TreeNode{
+					Val: 20,
+					Left: &tree.TreeNode{
+						Val: 15,
+					},
+					Right: &tree.TreeNode{
+						Val: 7,
+					},
+				},
+			},
+			expected: [][]int{{3}, {9, 20}, {15, 7}},
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d", tc.tree.List())
+		t.Run(name, func(t *testing.T) {
+			actual := levelOrder(tc.tree)
+			utils.Checkf(t, is.DeepEqual(tc.expected, actual), tc)
+		})
+	}
+}
