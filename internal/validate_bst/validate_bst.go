a0f0130a2e6a6ace39b452a27b157695508b4b81
ashtanko <shtankopro@gmail.com>
2021-03-07 11:42:15 +0200

    Solution of Validate Binary Search Tree problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/validate_bst/validate_bst.go
@@ -0,0 +1,28 @@
+package validate_bst
+
+import "github.com/ashtanko/go-algorithms/ds/tree"
+
+func isValidBST(root *tree.TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	res, v := true, root.Val
+	doValidateBST(&res, root.Left, &v, nil)
+	doValidateBST(&res, root.Right, nil, &v)
+	return res
+}
+
+func doValidateBST(res *bool, node *tree.TreeNode, upperBound, lowerBound *int) {
+	if node == nil || !*res {
+		return
+	}
+	if (upperBound != nil && node.Val >= *upperBound) ||
+		(lowerBound != nil && node.Val <= *lowerBound) {
+		*res = false
+		return
+	}
+
+	v := node.Val
+	doValidateBST(res, node.Left, &v, lowerBound)
+	doValidateBST(res, node.Right, upperBound, &v)
+}
---
+++ internal/validate_bst/validate_bst_test.go
@@ -0,0 +1,55 @@
+package validate_bst
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/ds/tree"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestValidateBinarySearchTree(t *testing.T) {
+	testCases := []struct {
+		root     *tree.TreeNode
+		expected bool
+	}{
+		{
+			root: &tree.TreeNode{
+				Val: 2,
+				Left: &tree.TreeNode{
+					Val: 1,
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			expected: true,
+		},
+		{
+			root: &tree.TreeNode{
+				Val: 5,
+				Left: &tree.TreeNode{
+					Val: 1,
+				},
+				Right: &tree.TreeNode{
+					Val: 4,
+					Left: &tree.TreeNode{
+						Val: 3,
+					},
+					Right: &tree.TreeNode{
+						Val: 6,
+					},
+				},
+			},
+			expected: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d", tc.root.List())
+		t.Run(name, func(t *testing.T) {
+			actual := isValidBST(tc.root)
+			utils.Checkf(t, is.Equal(tc.expected, actual), tc)
+		})
+	}
+}
