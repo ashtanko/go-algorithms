31cd2b0cec07d48c90f8ea1a2b148f230ea6bd2d
ashtanko <shtankopro@gmail.com>
2021-03-08 10:24:24 +0200

    Small refactor
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- internal/range_sum_bst/range_sum_bst.go
+++ internal/range_sum_bst/range_sum_bst.go
@@ -3,7 +3,5 @@ package range_sum_bst
 import "github.com/ashtanko/go-algorithms/ds/tree"
 
 func rangeSumBST(root *tree.TreeNode, low int, high int) int {
-	ans := 0
-	tree.Dfs(root, low, high, &ans)
-	return ans
+	return tree.Dfs(root, low, high)
 }
