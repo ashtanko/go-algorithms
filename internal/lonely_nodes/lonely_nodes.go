c8ff43f56160a427ee57ce40ef4779cf33887dd7
ashtanko <shtankopro@gmail.com>
2021-03-10 10:11:10 +0200

    Solution of Find All The Lonely Nodes problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/lonely_nodes/lonely_nodes.go
@@ -0,0 +1,23 @@
+package lonely_nodes
+
+import "github.com/ashtanko/go-algorithms/ds/tree"
+
+func getLonelyNodes(root *tree.TreeNode) []int {
+	return dfs(root, nil)
+}
+
+func dfs(root *tree.TreeNode, parent *tree.TreeNode) []int {
+	lonely := make([]int, 0)
+	if root == nil {
+		return lonely
+	}
+
+	if parent != nil && (parent.Left == nil || parent.Right == nil) {
+		lonely = append(lonely, root.Val)
+	}
+
+	lonely = append(lonely, dfs(root.Left, root)...)
+	lonely = append(lonely, dfs(root.Right, root)...)
+
+	return lonely
+}
---
+++ internal/lonely_nodes/lonely_nodes_test.go
@@ -0,0 +1,100 @@
+package lonely_nodes
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/ds/tree"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestLonelyNodes(t *testing.T) {
+	testCases := []struct {
+		tree     *tree.TreeNode
+		expected []int
+	}{
+		{
+			tree: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 2,
+					Right: &tree.TreeNode{
+						Val: 4,
+					},
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			expected: []int{4},
+		},
+		{
+			tree: &tree.TreeNode{
+				Val: 7,
+				Left: &tree.TreeNode{
+					Val: 1,
+					Left: &tree.TreeNode{
+						Val: 6,
+					},
+				},
+				Right: &tree.TreeNode{
+					Val: 4,
+					Left: &tree.TreeNode{
+						Val: 5,
+					},
+					Right: &tree.TreeNode{
+						Val: 3,
+						Right: &tree.TreeNode{
+							Val: 2,
+						},
+					},
+				},
+			},
+			expected: []int{6, 2},
+		},
+		{
+			tree: &tree.TreeNode{
+				Val: 11,
+				Left: &tree.TreeNode{
+					Val: 99,
+					Left: &tree.TreeNode{
+						Val: 77,
+						Left: &tree.TreeNode{
+							Val: 55,
+							Left: &tree.TreeNode{
+								Val: 33,
+							},
+						},
+					},
+				},
+				Right: &tree.TreeNode{
+					Val: 88,
+					Right: &tree.TreeNode{
+						Val: 66,
+						Right: &tree.TreeNode{
+							Val: 44,
+							Right: &tree.TreeNode{
+								Val: 22,
+							},
+						},
+					},
+				},
+			},
+			expected: []int{77, 55, 33, 66, 44, 22},
+		},
+		{
+			tree: &tree.TreeNode{
+				Val: 197,
+			},
+			expected: []int{},
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d", tc.tree.List())
+		t.Run(name, func(t *testing.T) {
+			actual := getLonelyNodes(tc.tree)
+			utils.Checkf(t, is.DeepEqual(tc.expected, actual), tc)
+		})
+	}
+}
