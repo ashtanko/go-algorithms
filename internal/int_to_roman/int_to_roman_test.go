b98bbc7ae44176fa42ecf455ce4e2ece8aaaaf8a
ashtanko <shtankopro@gmail.com>
2021-03-11 10:57:25 +0200

    Solution of Integer to Roman problem
    Signed-off-by: ashtanko <shtankopro@gmail.com>

---
+++ internal/int_to_roman/int_to_roman_test.go
@@ -0,0 +1,44 @@
+package int_to_roman
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestIntToRoman(t *testing.T) {
+	testCases := []struct {
+		num      int
+		expected string
+	}{
+		{
+			3,
+			"III",
+		},
+		{
+			4,
+			"IV",
+		},
+		{
+			9,
+			"IX",
+		},
+		{
+			58,
+			"LVIII",
+		},
+		{
+			1994,
+			"MCMXCIV",
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d", tc.num)
+		t.Run(name, func(t *testing.T) {
+			actual := intToRoman(tc.num)
+			utils.Checkf(t, is.Equal(tc.expected, actual), tc)
+		})
+	}
+}
