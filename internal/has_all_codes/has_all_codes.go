2ea6a6aabfa205deecc2b88a8ecb738e727b1eed
ashtanko <shtankopro@gmail.com>
2021-03-12 15:16:32 +0200

    Optimize has all codes solution
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- internal/has_all_codes/has_all_codes.go
+++ internal/has_all_codes/has_all_codes.go
@@ -1,15 +1,26 @@
 package has_all_codes
 
-import "math"
-
 func hasAllCodes(s string, k int) bool {
-	if len(s) < k {
-		return false
+	need := uint(1 << k)
+	got := make([]bool, need)
+	allOne := need - 1
+	hashVal := uint(0)
+
+	for i := 0; i < len(s); i++ {
+		b1 := hashVal << 1
+		b2 := b1 & allOne
+		b3 := uint(s[i] - '0')
+		b4 := b3 | b2
+		hashVal = b4
+
+		if i >= k-1 && !got[hashVal] {
+			got[hashVal] = true
+			need--
+			if need == 0 {
+				return true
+			}
+		}
 	}
-	hash := map[string]interface{}{}
-	for i := 0; i <= len(s)-k; i++ {
-		hash[s[i:i+k]] = nil
-	}
-	total := int(math.Pow(2, float64(k)))
-	return len(hash) == total
+
+	return false
 }
