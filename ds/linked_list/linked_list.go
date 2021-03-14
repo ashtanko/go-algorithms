18e46e9e93aa980bf357896b204a3b2c39c7cc52
ashtanko <shtankopro@gmail.com>
2021-03-14 13:47:41 +0200

    Add linked list utils
    Signed-off-by: ashtanko <shtankopro@gmail.com>

--- ds/linked_list/linked_list.go
+++ ds/linked_list/linked_list.go
@@ -1,8 +1,24 @@
 package linked_list
 
+import (
+	"fmt"
+	"strings"
+)
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
+}
+
+type Node struct {
+	prev *Node
+	next *Node
+	key  interface{}
+}
+
+type LinkedList struct {
+	head *Node
+	tail *Node
 }
 
 func FromSlice(nums []int) *ListNode {
@@ -30,4 +46,66 @@ func (list *ListNode) ToSlice() []int {
 		list = list.Next
 	}
 	return slice
+}
+
+func (L *LinkedList) Insert(key interface{}) {
+	list := &Node{
+		next: L.head,
+		key:  key,
+	}
+	if L.head != nil {
+		L.head.prev = list
+	}
+	L.head = list
+
+	l := L.head
+
+	for l.next != nil {
+		l = l.next
+	}
+	L.tail = l
+}
+
+func (L *LinkedList) Display() string {
+	list := L.head
+	b := strings.Builder{}
+	for list != nil {
+		b.WriteString(fmt.Sprintf("%+v -> ", list.key))
+		list = list.next
+	}
+	return b.String()
+}
+
+func (L *LinkedList) ShowBackwards() string {
+	list := L.tail
+	b := strings.Builder{}
+	for list != nil {
+		b.WriteString(fmt.Sprintf("%v <- ", list.key))
+		list = list.prev
+	}
+	return b.String()
+}
+
+func (L *LinkedList) Size() int {
+	size := 0
+	curr := L.head
+	for curr != nil {
+		curr = curr.next
+		size++
+	}
+	return size
+}
+
+func (L *LinkedList) Reverse() {
+	curr := L.head
+	var prev *Node
+	L.tail = L.head
+
+	for curr != nil {
+		next := curr.next
+		curr.next = prev
+		prev = curr
+		curr = next
+	}
+	L.head = prev
 }
--- ds/linked_list/linked_list_test.go
+++ ds/linked_list/linked_list_test.go
@@ -2,6 +2,7 @@ package linked_list
 
 import (
 	"github.com/ashtanko/go-algorithms/utils"
+	"github.com/stretchr/testify/assert"
 	is "gotest.tools/v3/assert/cmp"
 	"testing"
 )
@@ -18,4 +19,91 @@ func TestFromToSlice(t *testing.T) {
 
 		utils.Checkf(t, is.DeepEqual(slice, testCase), testCase)
 	}
+}
+
+func TestLinkedList_Insert(t *testing.T) {
+	t.Run("when empty", func(t *testing.T) {
+		list := LinkedList{}
+		assert.Equal(t, 0, list.Size())
+	})
+
+	t.Run("when full", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(4)
+		list.Insert(8)
+		list.Insert(15)
+		list.Insert(16)
+		list.Insert(23)
+		list.Insert(42)
+		assert.Equal(t, 6, list.Size())
+	})
+}
+
+func TestLinkedList_Display(t *testing.T) {
+	t.Run("when one element", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		expected := "1 -> "
+		actual := list.Display()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("when two elements", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		list.Insert(2)
+		expected := "2 -> 1 -> "
+		actual := list.Display()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("when 3 elements", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		list.Insert(2)
+		list.Insert(3)
+		expected := "3 -> 2 -> 1 -> "
+		actual := list.Display()
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestLinkedList_ShowBackwards(t *testing.T) {
+	t.Run("when one element", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		expected := "1 <- "
+		actual := list.ShowBackwards()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("when two elements", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		list.Insert(2)
+		expected := "1 <- 2 <- "
+		actual := list.ShowBackwards()
+		assert.Equal(t, expected, actual)
+	})
+
+	t.Run("when 3 elements", func(t *testing.T) {
+		list := LinkedList{}
+		list.Insert(1)
+		list.Insert(2)
+		list.Insert(3)
+		expected := "1 <- 2 <- 3 <- "
+		actual := list.ShowBackwards()
+		assert.Equal(t, expected, actual)
+	})
+}
+
+func TestLinkedList_Reverse(t *testing.T) {
+	list := LinkedList{}
+	list.Insert(1)
+	list.Insert(2)
+	list.Insert(3)
+	list.Reverse()
+	expected := "1 -> 2 -> 3 -> "
+	actual := list.Display()
+	assert.Equal(t, expected, actual)
 }
