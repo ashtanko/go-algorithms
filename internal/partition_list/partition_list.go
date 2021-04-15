49652c6ebe4088c03f08511e47cfc9b5a6bc94b4
Ivan Bozhitskyi <bojivan.pro@gmail.com>
2021-04-15 15:22:27 +0300

    Partition List
    Signed-off-by: Ivan Bozhitskyi <bojivan.pro@gmail.com>

---
+++ internal/partition_list/partition_list.go
@@ -0,0 +1,27 @@
+package partition_list
+
+import . "github.com/ashtanko/go-algorithms/ds/linked_list"
+
+func partition(head *ListNode, x int) *ListNode {
+	cur := head
+	var l1 []int
+	var l2 []int
+	for cur != nil {
+		v := cur.Val
+		if v < x {
+			l1 = append(l1, v)
+		} else {
+			l2 = append(l2, v)
+		}
+		cur = cur.Next
+	}
+	l := append(l1, l2...)
+	t := 0
+	curr := head
+	for curr != nil {
+		curr.Val = l[t]
+		t++
+		curr = curr.Next
+	}
+	return head
+}
---
+++ internal/partition_list/partition_list_test.go
@@ -0,0 +1,36 @@
+package partition_list
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/ds/linked_list"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+func TestPartition(t *testing.T) {
+	testCases := []struct {
+		head     *linked_list.ListNode
+		x        int
+		expected *linked_list.ListNode
+	}{
+		{
+			linked_list.FromSlice([]int{1, 4, 3, 2, 5, 2}),
+			3,
+			linked_list.FromSlice([]int{1, 2, 2, 4, 3, 5}),
+		},
+		{
+			linked_list.FromSlice([]int{2, 1}),
+			2,
+			linked_list.FromSlice([]int{1, 2}),
+		},
+	}
+
+	for _, tc := range testCases {
+		name := fmt.Sprintf("%d %d", tc.head.ToSlice(), tc.head.ToSlice())
+		t.Run(name, func(t *testing.T) {
+			actual := partition(tc.head, tc.x).ToSlice()
+			utils.Checkf(t, is.DeepEqual(tc.expected.ToSlice(), actual), tc)
+		})
+	}
+}
