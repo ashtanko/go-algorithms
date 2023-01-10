22d57614187d2d86000b3b48bc065ff84a414ff3
Oleksii Shtanko <shtankopro@gmail.com>
2023-01-10 08:58:29 +0200

    Solution of Same Tree problem

---
+++ internal/same_tree/same_tree.go
@@ -0,0 +1,49 @@
+package same_tree
+
+import "github.com/ashtanko/go-algorithms/ds/tree"
+
+// 100. Same Tree
+// https://leetcode.com/problems/same-tree/description/
+func isSameTreeRecursive(p *tree.TreeNode, q *tree.TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	}
+
+	if p.Val != q.Val {
+		return false
+	}
+	return isSameTreeRecursive(p.Left, q.Left) && isSameTreeRecursive(p.Right, q.Right)
+}
+
+func isSameTreeBFS(p *tree.TreeNode, q *tree.TreeNode) bool {
+	qP := []*tree.TreeNode{p}
+	qQ := []*tree.TreeNode{q}
+
+	for len(qP) != 0 && len(qQ) != 0 {
+		pNode := qP[0]
+		qP = qP[1:]
+
+		qNode := qQ[0]
+		qQ = qQ[1:]
+
+		if pNode == nil && qNode == nil {
+			continue
+		}
+		if pNode == nil && qNode != nil || pNode != nil && qNode == nil {
+			return false
+		}
+		if pNode.Val != qNode.Val {
+			return false
+		}
+
+		qP = append(qP, pNode.Left, pNode.Right)
+		qQ = append(qQ, qNode.Left, qNode.Right)
+	}
+
+	if len(qP) == 0 && len(qQ) == 0 {
+		return true
+	}
+	return false
+}
---
+++ internal/same_tree/same_tree_test.go
@@ -0,0 +1,115 @@
+package same_tree
+
+import (
+	"fmt"
+	"github.com/ashtanko/go-algorithms/ds/tree"
+	"github.com/ashtanko/go-algorithms/utils"
+	is "gotest.tools/v3/assert/cmp"
+	"testing"
+)
+
+type testCase struct {
+	p        *tree.TreeNode
+	q        *tree.TreeNode
+	expected bool
+}
+
+func TestSameTree(t *testing.T) {
+
+	inputData := []testCase{
+		{
+			p: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 2,
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			q: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 2,
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			expected: true,
+		},
+		{
+			p: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 2,
+				},
+				Right: &tree.TreeNode{
+					Val: 3,
+				},
+			},
+			q: &tree.TreeNode{
+				Val: 1,
+				Left: &tree.TreeNode{
+					Val: 2,
+				},
+				Right: &tree.TreeNode{
+					Val: 4,
+				},
+			},
+			expected: false,
+		},
+		{
+			p: &tree.TreeNode{
+				Val: 1,
+			},
+			q: &tree.TreeNode{
+				Val: 1,
+			},
+			expected: true,
+		},
+		{
+			p: &tree.TreeNode{
+				Val: 1,
+			},
+			q:        &tree.TreeNode{},
+			expected: false,
+		},
+		{
+			p: &tree.TreeNode{},
+			q: &tree.TreeNode{
+				Val: 1,
+			},
+			expected: false,
+		},
+		{
+			p:        &tree.TreeNode{},
+			q:        &tree.TreeNode{},
+			expected: true,
+		},
+	}
+
+	testCases := []struct {
+		strategy     func(p *tree.TreeNode, q *tree.TreeNode) bool
+		strategyName string
+	}{
+		{
+			strategy:     isSameTreeRecursive,
+			strategyName: "recursive",
+		},
+		{
+			strategy:     isSameTreeBFS,
+			strategyName: "BFS",
+		},
+	}
+
+	for _, tc := range testCases {
+		for _, i := range inputData {
+			name := fmt.Sprintf("%s %d %d", tc.strategyName, i.p.List(), i.q.List())
+			t.Run(name, func(t *testing.T) {
+				actual := tc.strategy(i.p, i.q)
+				utils.Checkf(t, is.DeepEqual(i.expected, actual), tc)
+			})
+		}
+	}
+}
